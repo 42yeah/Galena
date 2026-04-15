31fa0c7084a69ee05a0a0d0273fc1e90c3b44f40
42yeah <hey@42yeah.is>
2026-04-16 00:10:00 +0800

    Added game folder

---
+++ game/src/GGameInstance.cpp
@@ -0,0 +1,13 @@
+#include "Galena/GVersion.h"
+
+#include <iostream>
+
+namespace galena {
+
+int main()
+{
+    std::cout << "Version: " << Version() << std::endl;
+    return 0;
+}
+
+}  // namespace galena
